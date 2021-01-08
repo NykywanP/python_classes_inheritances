0dcace5854ac2150d4418e71f0f6226428a8438c
Nykywan Parker <nykywanparker@gmail.com>
2021-01-08 11:48:45 -0500

    [dogs.py] add object by file

---
+++ dogs.py
@@ -0,0 +1,26 @@
+# class
+
+class Dogs():
+    """ This is the type of dogs I like """
+
+    def__init__(self, breed, size, temperament):
+    self.breed = breed
+    self.size = size
+    self.temperament
+
+    def__str__(self):
+    return "{}", {}, {}".format(self.breed, self.size, self.temperament)
+
+    def rottweiler(self):
+        self.large
+
+    def pitbull(self):
+        self.medium
+
+    def pug(self):  
+        self.small      
+
+
+dog1.rottweiler()   
+dog2.pitbull()   
+dog3.pug()  
